8323b8b70d5fc7d2975b8fe169a1862e1687b57b
Jabuitroon <Jandresbuiitron@gmail.com>
2026-03-24 21:50:42 -0500

    Suspense for loginpage

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from 'react'
 import { LoginFormWrapper } from './login-form-wrapper'
 
 export default function LoginPage() {
-  return <LoginFormWrapper />
+  return (
+    <Suspense
+      fallback={<div className='bg-muted h-40 animate-pulse rounded-md' />}
+    >
+      <LoginFormWrapper />
+    </Suspense>
+  )
 }
